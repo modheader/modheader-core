603d8e7ca11049f3611b005d8e46336c4bd95eac
Hao Nguyen <hao1300@gmail.com>
2022-07-17 11:10:52 -0500

    Add auto-sync icon

--- src/svelte/ProfileBadge.svelte
+++ src/svelte/ProfileBadge.svelte
@@ -1,17 +1,28 @@
 <script>
-  import { mdiCheckboxMarkedCircle, mdiCheckboxBlankCircle } from '@mdi/js';
+  import { mdiCheckboxMarkedCircle, mdiCheckboxBlankCircle, mdiSync, mdiSyncOff } from '@mdi/js';
   import MdiIcon from './MdiIcon.svelte';
   import { selectedProfile } from '../js/profile.js';
+  import { PRIMARY_COLOR } from '../js/constants.js';
+  import { Status } from '../js/profile-sync.js';
 
   export let profile;
 
   $: isActive = profile.alwaysOn || profile === $selectedProfile;
+  $: isAutoSyncActive = profile.liveProfileStatus === Status.ACTIVE;
 </script>
 
 <span class="profile-badge-icon-container" style="background: {profile.backgroundColor}">
   <span class="profile-badge-icon-text" style="color: {profile.textColor}">
     {profile.shortTitle}
   </span>
+  {#if profile.liveProfileUrl}
+    <MdiIcon
+      class="profile-badge-autosync-status"
+      size="12"
+      icon={isAutoSyncActive ? mdiSync : mdiSyncOff}
+      color={isAutoSyncActive ? PRIMARY_COLOR : 'red'}
+    />
+  {/if}
   <MdiIcon
     class="profile-badge-status"
     size="12"
